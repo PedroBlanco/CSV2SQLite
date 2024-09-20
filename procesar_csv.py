36e0a289fa7e0776a7dd694bff7ee0cfcacd1c4b
Pedro Blanco <9304344+PedroBlanco@users.noreply.github.com>
2024-09-20 13:26:19 +0200

    feat: Añado nuevas opciones
    Opciones:
    * Progreso mejorado
    * Modo verbose
      * Muestro datos de archivos a usar, parámetros y número y texto de los campos del CSV

--- procesar_csv.py
+++ procesar_csv.py
@@ -10,23 +10,36 @@ import sqlite3
 import argparse
 
 # Define los argumentos de entrada
-parser = argparse.ArgumentParser(description='Procesa un archivo CSV y lo convierte en una base de datos SQLite')
+parser = argparse.ArgumentParser(description=f'{sys.argv[0]}: Procesa un archivo CSV y lo convierte en una base de datos SQLite', add_help=False)
 parser.add_argument('archivo_csv', help='Nombre del archivo CSV a procesar')
+parser.add_argument('-h', '--help', action='help', help='Muestra esta ayuda y sale')
 parser.add_argument('--db_name', help='Nombre del archivo de base de datos SQLite a generar (por defecto, se genera a partir del nombre del archivo CSV)')
 parser.add_argument('-f', '--force', action='store_true', help='Reescribe el archivo de base de datos si ya existe')
-parser.add_argument('--chunksize', default=1000, type=int, help='Número de filas a procesar por parte')
-parser.add_argument('--progress', default=False, type=int, help='Muestra el progreso de la inserción de filas')
+parser.add_argument('-c','--chunksize', default=1000, type=int, help='Número de filas a procesar por parte')
+parser.add_argument('-p','--progress', action='store_true', help='Muestra el progreso de la inserción de filas')
+parser.add_argument('-v','--verbose',  action='store_true', help='Muestra información de todo el proceso')
 
 args = parser.parse_args()
 
+if args.verbose:
+    args.progress = True
+
 # Obtiene el nombre del archivo CSV y el nombre del archivo de base de datos SQLite
 csv_file = args.archivo_csv
+
+if args.verbose:
+    print (f"[VERBOSE] Archivo CSV: {csv_file}")
+    print (f"[VERBOSE] Chunksize (número de filas a procesar por parte): {args.chunksize}")
+
 # Genera el nombre del archivo de base de datos a partir del nombre del archivo CSV
 db_file = args.db_name or f'{args.archivo_csv[:-4]}.db'
 
+if args.verbose:
+    print (f"[VERBOSE] Archivo de base de datos: {db_file}")
+    
 # Verifica si el archivo de base de datos ya existe y si se debe reescribir
 if os.path.exists(db_file) and not args.force:
-    print(f'El archivo de base de datos {db_file} ya existe. Use --force para reescribirlo.')
+    print(f'[ERROR] El archivo de base de datos {db_file} ya existe. Use --force para reescribirlo.')
     sys.exit(1)
 
 # Conecta a la base de datos SQLite
@@ -35,8 +48,16 @@ cursor = conn.cursor()
 
 # Abre el archivo CSV
 with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfile:
-    num_columns = 63
-
+    reader = csv.reader(csvfile, delimiter=',', quotechar='"',doublequote=True)
+    cabecera = next(reader)
+    num_columns = len(cabecera)
+    # num_columns = 63
+    
+    if args.verbose:
+        print(f"[VERBOSE] Número de campos: {len(cabecera)}")
+        print(f"[VERBOSE] Campos: {cabecera}")
+    # sys.exit(1)
+    
     # Crea la tabla en la base de datos
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS mi_tabla (
@@ -44,14 +65,18 @@ with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfil
             {}
         )
     '''.format(', '.join(['columna{} TEXT'.format(i) for i in range(1, num_columns + 1)])))
-    
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"',doublequote=True)
-    next(reader)  # Salta la primera fila (encabezados)
 
-    print ( f'Insertando filas (de {args.chunksize} en {args.chunksize}) en la base de datos (use --progress para mostrar el progreso): ' )
+    if args.progress:
+        sys.stdout.write ( f'[INFO] Insertando filas (de {args.chunksize} en {args.chunksize}) en la base de datos: _' )
+    else:
+        sys.stdout.write ( f'[INFO] Insertando filas (de {args.chunksize} en {args.chunksize}) en la base de datos (use --progress para mostrar el progreso): _' )
+    sys.stdout.flush()
+
     # Procesa el archivo CSV por partes
     chunk = []
     num_lines = 0
+    caracter_progreso = ['/', '-', '\\', '|']
+
     for fila in reader:
         chunk.append(fila)
         if len(chunk) >= args.chunksize:
@@ -59,7 +84,9 @@ with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfil
             cursor.executemany('INSERT INTO mi_tabla VALUES (NULL, {})'.format(', '.join(['?'] * num_columns)), chunk)
             num_lines += len(chunk)
             if args.progress:
-                print ( '+', end='' )
+                sys.stdout.write('\b' + caracter_progreso[num_lines % 4] )
+                # print ( '+', end='' )
+                sys.stdout.flush()
             chunk = []
 
     # Inserta los datos restantes en la base de datos
@@ -67,8 +94,8 @@ with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfil
         cursor.executemany('INSERT INTO mi_tabla VALUES (NULL, {})'.format(', '.join(['?'] * num_columns)), chunk)
         num_lines += len(chunk)
         if args.progress:
-            print ( '+' )
-        print ( f'Se han insertado {num_lines} filas en la base de datos.' )
+            print ( '\b+' )
+        print ( f'[INFO] Se han insertado {num_lines} filas en la base de datos.' )
         
 
 # Cierra la conexión a la base de datos
