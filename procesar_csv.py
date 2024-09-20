e604d3f54c88ce87d8493dea6a77fcf35c67945d
Pedro Blanco <9304344+PedroBlanco@users.noreply.github.com>
2024-09-20 14:27:32 +0200

    feat: Mejoro/arreglo la gestión de archivos existentes de BD
    Además muestro información:
    * Tiempo de ejecución
    * Definición de la tabla creada
    
    Cambio líneas procesadas por defecto (de 1000 a 50)

--- procesar_csv.py
+++ procesar_csv.py
@@ -6,6 +6,7 @@
 import os
 import sys
 import csv
+import time
 import sqlite3
 import argparse
 
@@ -15,11 +16,14 @@ parser.add_argument('archivo_csv', help='Nombre del archivo CSV a procesar')
 parser.add_argument('-h', '--help', action='help', help='Muestra esta ayuda y sale')
 parser.add_argument('--db_name', help='Nombre del archivo de base de datos SQLite a generar (por defecto, se genera a partir del nombre del archivo CSV)')
 parser.add_argument('-f', '--force', action='store_true', help='Reescribe el archivo de base de datos si ya existe')
-parser.add_argument('-c','--chunksize', default=1000, type=int, help='Número de filas a procesar por parte')
+parser.add_argument('-c','--chunksize', default=50, type=int, help='Número de filas a procesar por parte, en caso de archivos grandes (50 por defecto)')
 parser.add_argument('-p','--progress', action='store_true', help='Muestra el progreso de la inserción de filas')
 parser.add_argument('-v','--verbose',  action='store_true', help='Muestra información de todo el proceso')
 
 args = parser.parse_args()
+
+# Inicia el cronometro
+start_time = time.time()
 
 if args.verbose:
     args.progress = True
@@ -38,9 +42,14 @@ if args.verbose:
     print (f"[VERBOSE] Archivo de base de datos: {db_file}")
     
 # Verifica si el archivo de base de datos ya existe y si se debe reescribir
-if os.path.exists(db_file) and not args.force:
-    print(f'[ERROR] El archivo de base de datos {db_file} ya existe. Use --force para reescribirlo.')
-    sys.exit(1)
+if os.path.exists(db_file):
+    if args.force:
+        os.remove(db_file)
+        if args.verbose:
+            print(f'[VERBOSE] El archivo existente de base de datos {db_file} ha sido eliminado (se ha usado la opción --force).')
+    else:
+        print(f'[ERROR] El archivo de base de datos {db_file} ya existe. Use --force para reescribirlo.')
+        sys.exit(1)
 
 # Conecta a la base de datos SQLite
 conn = sqlite3.connect(args.db_name)
@@ -51,21 +60,24 @@ with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfil
     reader = csv.reader(csvfile, delimiter=',', quotechar='"',doublequote=True)
     cabecera = next(reader)
     num_columns = len(cabecera)
-    # num_columns = 63
     
     if args.verbose:
         print(f"[VERBOSE] Número de campos: {len(cabecera)}")
         print(f"[VERBOSE] Campos: {cabecera}")
-    # sys.exit(1)
     
     # Crea la tabla en la base de datos
-    cursor.execute('''
+    definicion_tabla = '''
         CREATE TABLE IF NOT EXISTS mi_tabla (
             id INTEGER PRIMARY KEY,
             {}
         )
-    '''.format(', '.join(['columna{} TEXT'.format(i) for i in range(1, num_columns + 1)])))
-
+    '''.format(', '.join([f'"{campo}" TEXT' for campo in cabecera]))
+    
+    cursor.execute( definicion_tabla )
+    
+    if args.verbose:
+        sys.stdout.write ( f'[VERBOSE] Creando tabla en la base de datos: {definicion_tabla}' )
+    
     if args.progress:
         sys.stdout.write ( f'[INFO] Insertando filas (de {args.chunksize} en {args.chunksize}) en la base de datos: _' )
     else:
@@ -96,7 +108,13 @@ with open(args.archivo_csv, 'r', buffering=1024*1024,encoding='utf-8') as csvfil
         if args.progress:
             print ( '\b+' )
         print ( f'[INFO] Se han insertado {num_lines} filas en la base de datos.' )
-        
+
+# Detiene el cronometro
+end_time = time.time()
+
+# Imprime el tiempo de ejecución
+if args.progress:
+    print (f'[INFO] Tiempo de ejecución: {end_time - start_time:.2f} segundos')
 
 # Cierra la conexión a la base de datos
 conn.commit()
